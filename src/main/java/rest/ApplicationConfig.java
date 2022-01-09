fb37dd7fea963f28d1b32f478b42161d90731654
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2022-01-09 17:29:28 +0100

    register endpoint

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.AdminResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
+        resources.add(security.RegisterEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
         resources.add(security.errorhandling.AuthenticationExceptionMapper.class);
         resources.add(security.errorhandling.NotAuthorizedExceptionMapper.class);
